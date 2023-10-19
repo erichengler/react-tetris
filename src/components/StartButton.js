a8c779c25211916ad750238fa1cf332c2a290ac6
Erich Engler <erich.engler@hotmail.com>
2023-10-19 16:49:38 -0500

    created gameHelpers.js - stage is an array of rows, row is an array of cells

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const StartButton = () => (
     <div>
         Start Game
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { createStage } from "../gameHelpers";
 // Components
 import Display from "./Display";
 import Stage from "./Stage";
@@ -7,7 +7,7 @@ import StartButton from "./StartButton";
 const Tetris = () => {
   return (
     <div>
-      <Stage />
+      <Stage stage={createStage()} />
       <aside>
         <div>
           <Display text="Score" />
