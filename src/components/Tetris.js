83fbf1bb1d9acb5ddefbdceecbe94af8da26be75
Erich Engler <erich.engler@hotmail.com>
2023-10-25 14:55:15 -0500

    fixed bug in randomTetromino function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,22 +1,37 @@
-import { createStage } from "../gameHelpers";
+import { useState } from "react";
 // Components
 import Display from "./Display";
 import Stage from "./Stage";
 import StartButton from "./StartButton";
 // Styled components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
+// Custom hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {/* if game is over, display game over */}
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+          // if game is not over, display the score, rows and level
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </StyledTetris>
