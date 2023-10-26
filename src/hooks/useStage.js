3cbf195383732a7f2e338889eecd22ca826e152c
Erich Engler <erich.engler@hotmail.com>
2023-10-26 17:32:17 -0500

    created sweepRows function in useStage.js, full rows are now removed from stage and an empty row is created at the top of the stage for each row swept

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,24 @@ import { createStage } from "../gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = newStage => 
+      // acc = new array to add empty rows to
+      newStage.reduce((acc, row) => {
+        // findIndex returns -1 if it doesn't find a match
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, [])
+
     const updateStage = (prevStage) => {
       // First flush the stage
       const newStage = prevStage.map((row) =>
@@ -26,6 +42,8 @@ export const useStage = (player, resetPlayer) => {
       // Then check if we collided with anything
       if (player.collided) {
         resetPlayer();
+        // Returns new stage after sweeping rows
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -34,5 +52,5 @@ export const useStage = (player, resetPlayer) => {
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
